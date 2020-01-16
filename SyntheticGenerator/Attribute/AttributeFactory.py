b20bf4c0fccd89450b3fc4aee2fcc181fafc5e74
Duncan McKinnon <13010582+duncankmckinnon@users.noreply.github.com>
2020-01-16 10:13:30 -0800

    Adding pydocs and representation

--- SyntheticGenerator/Attribute/AttributeFactory.py
+++ SyntheticGenerator/Attribute/AttributeFactory.py
@@ -1,8 +1,23 @@
 from Attribute import Attribute
 from Distribution.DistributionFactory import DistributionFactory
 
-
 class AttributeFactory:
+    """
+    Class to generate and deploy specific attributes that 
+    will share a distribution factory. Attributes types are:
+    - custom
+    - boolean
+    - string
+    - int or int64
+    - float or float64
+    
+    Properties:
+        - central_distribution_factory (DistributionFactory) - a DistributionFactory
+    Implements:
+        - create(name -> str, dtype -> str) --> Attribute()
+    Raises:
+        - AttributeError - dtype is not valid
+    """
     def __init__(self):
         self.central_distribution_factory = DistributionFactory()
     def create(self, name, dtype, drange = None):
@@ -18,5 +33,9 @@ class AttributeFactory:
         elif dtype == 'custom':
             attribute_class = Attribute.Attribute(name, self.central_distribution_factory)
         else:
-            raise AttributeError('dtype is not valid (must be one of [boolean, string, int64, float64, custom])')
+            raise AttributeError('dtype is not valid (must be one of [boolean, string, int, int64, float, float64, custom])')
         return attribute_class
+    def __str__(self):
+        return 'AttributeFactory()'
+    def __rep__(self):
+        return self.__str__()
