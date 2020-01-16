5a69401d6920e640af30f998b8ed2f8d511fe811
Duncan McKinnon <13010582+duncankmckinnon@users.noreply.github.com>
2020-01-16 12:40:36 -0800

    Updating example for coverage

--- SyntheticGenerator/main.py
+++ SyntheticGenerator/main.py
@@ -4,30 +4,37 @@ from DataSynthesizer import DataSynthesizer, DataLabeler
 if __name__ == "__main__":
     SD = DataSynthesizer.DataSynthesizer(rows=10)
     data = SD.generate([{
-        'name': 'BoolTest',
+        'name': 'BoolPowerTest',
         'dtype': 'boolean',
         'distribution': 'power',
         'sparsity': 0.5,
         'kwargs': {'a': 0.2}
     }, {
-        'name': 'FloatTest',
+        'name': 'FloatExpTest',
         'dtype': 'float',
         'distribution': 'exponential',
         'drange': [1/(i*10+3) for i in range(1, 101)],
         'sparsity': 0.2,
         'kwargs': {'b': 3}
-    }, {
-        'name': 'StringTest',
+    },{
+        'name': 'StringCustomTest',
         'dtype': 'string',
-        'distribution': 'normal',
+        'distribution': 'custom',
         'drange': ['alpha', 'beta', 'omega', 'epsilon', 'delta'],
         'sparsity': 0.05,
         'kwargs': {'weights': [0.2, 0.4, 0.1, 0.05, 0.2]}
     }, {
-        'name': 'IntegerTest',
+        'name': 'CustomNormalTest',
+        'dtype': 'custom',
+        'distribution': 'normal',
+        'drange': ['alpha', 'beta', 'omega', 3, 'delta'],
+        'sparsity': 0.5,
+        'kwargs': {'scale' : 2}
+    }, {
+        'name': 'IntegerUniformTest',
         'dtype': 'int64',
         'distribution': 'uniform',
-        'drange': [1, 3, 5, 7, 9, 11, 14],
-        'sparsity': 0.9
+        'drange': [i for i in range(-27,28,3)],
+        'sparsity': 0.8
     }])
     print(data)
